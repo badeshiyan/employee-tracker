02b3ff57026394c5bedd895ec7cb75d7bc624d30
Adebambo Adeshiyan <badeshiyan@gmail.com>
2020-11-24 21:34:58 -0500

    fleshed out prompt coding with switch info and db functions

--- index.js
+++ index.js
@@ -10,24 +10,94 @@ let connection = mysql.createConnection({
   database: "employees_db",
 });
 
-// connection.query("select * from employee", function (err, res) {
-//   if (err) throw err;
-//   console.table(res);
-// });
+// connect to the mysql server and sql database
+connection.connect(function (err) {
+  if (err) throw err;
+  console.log("connected as id " + connection.threadId);
+});
 
 function startApp() {
   inquirer
     .prompt([
       {
         type: "list",
-        message: "Select what type of employee you would like to add",
+        message: "what would you like to do?",
         name: "userchoice",
-        choices: ["manager", "engineer", "intern", "build team"],
+        choices: [
+          "View all employees",
+          "Add employee",
+          "Add role",
+          "Add department",
+          "View all roles",
+          "View all departments",
+          "Exit",
+        ],
       },
     ])
-    .then(function (res) {
-      console.log(res);
+    .then(function (Response) {
+      switch (Response.userchoice) {
+        case "View all employees":
+          console.log("you chose to view all employees");
+          viewAllEmployees();
+          break;
+        case "Add employee":
+          console.log("you chose to add an employee");
+          addEmployee();
+          break;
+        case "Add role":
+          console.log("you chose to add role");
+          addRole();
+          break;
+        case "Add department":
+          console.log("you chose to add department");
+          addDepartment();
+          break;
+        case "View all roles":
+          console.log("you chose to view all roles");
+          viewAllRoles();
+          break;
+        case "View all departments":
+          console.log("viewing all departments");
+          viewAllDepartments();
+          startApp();
+          break;
+        case "Exit":
+          connection.end();
+      }
     });
 }
 
 startApp();
+
+function viewAllDepartments() {
+  connection.query("SELECT department.name FROM department;", function (
+    err,
+    res
+  ) {
+    if (err) throw err;
+    console.log(res);
+    connection.end();
+  });
+}
+
+function viewAllRoles() {
+  connection.query(
+    "SELECT role.title, role.salary, department.name FROM role LEFT JOIN department	ON role.department_id = department.id;",
+    function (err, res) {
+      if (err) throw err;
+      console.log(res);
+      connection.end();
+    }
+  );
+}
+
+function viewAllEmployees() {
+  connection.query(
+    "SELECT employee.first_name, employee.last_name, role.title, role.salary, department.name FROM employee LEFT  JOIN role on employee.role_id = role.id LEFT JOIN department ON role.department_id = department.id;",
+    function (err, res) {
+      if (err) throw err;
+      console.log(res);
+      connection.end();
+    }
+  );
+}
